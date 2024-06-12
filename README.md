5e3b198468e33fd24a2f225983a4515267365479
duelingbenjos <getrektwithme1@gmail.com>
2024-06-12 23:28:52 +0100

    update sandbox env

--- README.md
+++ README.md
@@ -1,49 +1,7 @@
-# Dice Game
 
-Example Smart Contract based Dice game with random functions and house edge. Range based dice with multiplier winnings based on range.
+# Example Contract : Dice Game
+This sandbox contains example contracts and tests demonstrating how to build a simple dice game & tests using xian-contracting.
 
-## Installation for Tests
+## To run tests :
 
-### Ubuntu 22.04
-
-Follow these steps to set up the environment on Ubuntu 22.04:
-
-1. **Update and prepare the system:**
-
-   ```bash
-   sudo apt-get update
-   sudo add-apt-repository ppa:deadsnakes/ppa -y
-   sudo apt-get update
-   ```
-
-2. **Install necessary packages:**
-
-   ```bash
-   sudo apt-get install pkg-config python3.11 python3.11-dev python3.11-venv libhdf5-dev build-essential
-   ```
-
-3. **Clone Contracting:**
-
-   ```bash
-   git clone https://github.com/xian-network/xian-contracting
-   git clone https://github.com/xian-network/dice-game.git
-   ```
-
-4. **Set up Python virtual environment and dependencies:**
-
-   ```bash
-   python3.11 -m venv xian_venv
-   source xian_venv/bin/activate
-   pip install -e xian-contracting/
-   ```
-
-5. **Run the tests**
-
-   ```bash
-   cd dice-game/tests
-   python3.11 test_dice.py
-   ```
-
-## Deploy
-
-Use the wallet to submit the con_uberdice.py contract contents.
+pytest tests/test_dice.py
--- tests/test_dice.py
+++ tests/test_dice.py
@@ -1,30 +1,21 @@
 import unittest
 from contracting.client import ContractingClient
-import random
-import string
-import os
-import sys
 
-# Get the directory where the script is located
-script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-# Change the current working directory
-os.chdir(script_dir)
-
-class MyTestCase(unittest.TestCase):
+class TestDice(unittest.TestCase):
 
     def setUp(self):
         self.c = ContractingClient()
         self.c.flush()
 
-        with open('../contracts/currency.py') as f:
+        with open('./contracts/currency.py') as f:
             code = f.read()
             self.c.submit(code, name='currency',
                           constructor_args={'vk': 'boi'})
 
         self.currency = self.c.get_contract('currency')
 
-        with open('../contracts/con_uberdice.py') as f:
+        with open('./contracts/con_uberdice.py') as f:
             code = f.read()
             self.c.submit(code, name='con_uberdice', signer='boi')
 
@@ -33,26 +24,30 @@ class MyTestCase(unittest.TestCase):
     def test_roll_loss(self):
         self.currency.transfer(amount=1000, to='con_uberdice', signer='boi')
         self.currency.approve(amount=50, to='con_uberdice', signer='boi')
-        self.uberdice.roll(bet_size=50, token_contract='currency', roll_type='under',roll_target=10, signer='boi',environment={
-	        'block_height': 5,
-	        'block_hash': 'loser'
-	    }) # Environment makes the roll a predictable 83, which is over 10 so it's a loss.
-        self.assertEqual(self.currency.balance_of(account='con_uberdice'), 1050) # Lose, so house gets 50.
+        self.uberdice.roll(bet_size=50, token_contract='currency', roll_type='under', roll_target=10, signer='boi', environment={
+            'block_height': 5,
+            'block_hash': 'loser'
+        })  # Environment makes the roll a predictable 83, which is over 10 so it's a loss.
+        # Lose, so house gets 50.
+        self.assertEqual(self.currency.balance_of(
+            account='con_uberdice'), 1050)
 
     def test_roll_win(self):
         self.currency.transfer(amount=1000, to='con_uberdice', signer='boi')
         self.currency.approve(amount=50, to='con_uberdice', signer='boi')
-        (self.uberdice.roll(bet_size=50, token_contract='currency', roll_type='over',roll_target=80, signer='boi',environment={
+        (self.uberdice.roll(bet_size=50, token_contract='currency', roll_type='over', roll_target=80, signer='boi', environment={
             'block_height': 5,
             'block_hash': 'loser'
-        })) # Environment makes the roll a predictable 83, which is over 80 so it's a win.
-        self.assertEqual(int(self.currency.balance_of(account='con_uberdice')),819) # Win, so house loses the multiplier of 50.
+        }))  # Environment makes the roll a predictable 83, which is over 80 so it's a win.
+        # Win, so house loses the multiplier of 50.
+        self.assertEqual(
+            int(self.currency.balance_of(account='con_uberdice')), 819)
 
     def test_roll_win_cant_cover(self):
         self.currency.transfer(amount=200, to='con_uberdice', signer='boi')
         self.currency.approve(amount=50, to='con_uberdice', signer='boi')
         with self.assertRaises(AssertionError):
-            self.uberdice.roll(bet_size=50, token_contract='currency', roll_type='over',roll_target=82, signer='boi',environment={
+            self.uberdice.roll(bet_size=50, token_contract='currency', roll_type='over', roll_target=82, signer='boi', environment={
                 'block_height': 5,
                 'block_hash': 'loser'
             })
@@ -62,20 +57,23 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.uberdice.game['owner'], 'new_owner')
 
     def test_change_allowed_tokens(self):
-        self.uberdice.change_allowed_tokens(tokens=['currency', 'other_token'], signer='boi')
-        self.assertEqual(self.uberdice.game['allowed_tokens'], ['currency', 'other_token'])
+        self.uberdice.change_allowed_tokens(
+            tokens=['currency', 'other_token'], signer='boi')
+        self.assertEqual(self.uberdice.game['allowed_tokens'], [
+                         'currency', 'other_token'])
         with self.assertRaises(AssertionError):
-            self.uberdice.roll(bet_size=50, token_contract='not_allowed', roll_type='over', roll_target=50, signer='boi',environment={
+            self.uberdice.roll(bet_size=50, token_contract='not_allowed', roll_type='over', roll_target=50, signer='boi', environment={
                 'block_height': 5,
                 'block_hash': 'loser'
             })
 
     def test_change_max_token_bet(self):
-        self.uberdice.change_max_token_bet(token_contract='currency', max_bet=100, signer='boi')
+        self.uberdice.change_max_token_bet(
+            token_contract='currency', max_bet=100, signer='boi')
         self.assertEqual(self.uberdice.game['max_token_bet', 'currency'], 100)
-        
+
         with self.assertRaises(AssertionError):
-            self.uberdice.roll(bet_size=101, token_contract='currency', roll_type='over', roll_target=50, signer='boi',environment={
+            self.uberdice.roll(bet_size=101, token_contract='currency', roll_type='over', roll_target=50, signer='boi', environment={
                 'block_height': 5,
                 'block_hash': 'loser'
             })
